3b21e80eae0f2c57c4048e0abe42cb529220bae3
Juan Ariza Ordonez <jariza-o@c2r3s1.42malaga.com>
2024-07-01 17:11:07 +0200

    Wordpress WORKING, ALL Working

--- srcs/requirements/wordpress/tools/use_wp.sh
+++ srcs/requirements/wordpress/tools/use_wp.sh
@@ -28,12 +28,14 @@ WP=/usr/local/bin/wp/wp-cli.phar
 
     # Config Wordpress
     if [ ! -e "/var/www/wordpress/wp-config.php" ]; then
+        sleep 20
         echo "${BLUE}Configuring Wordpress ${NC}"
         ${WP} config create --allow-root --dbname=$MYSQL_DATABASE_NAME --dbuser=$MYSQL_USER \
                         --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME:3306 \
                         --path='/var/www/wordpress/'
         ${WP} core install     --url=$DOMAIN_NAME --title=$WORDPRESS_SITE_TITLE --admin_user=$WORDPRESS_ROOT_USER --admin_password=$WORDPRESS_ROOT_PASSWORD --admin_email=$WORDPRESS_ROOT_EMAIL --allow-root --path='/var/www/wordpress/'
-        # ${WP} user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress/' # Puedo Poner >> log.txt
+        #${WP} user create      --role=author $WORDPRESS_USER $WORDPRESS_EMAIL --user_pass=$WORDPRESS_PASSWORD --path='/var/www/wordpress/' # Puedo Poner >> log.txt
+        ${WP} user create      $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_PASSWORD --allow-root --path='/var/www/wordpress/'
         echo "${GREEN}WordPress Configured${NC}"
     fi
 
